0533b4c4159507e14bc9268f0e5fbdfd21edf47a
wara <kusabashira227@gmail.com>
2015-08-12 22:34:13 +0900

    FIx indent

--- scmlib/fasttemplate.scm
+++ scmlib/fasttemplate.scm
@@ -48,77 +48,78 @@
             (rxmatch-substring m)))
         (reverse unsafe-tag-ls)))
 
-  (define (input-value-with-dialog from-tag)
-    (let ((to-tag (app-input-box from-tag)))
-      (if to-tag
-        (cons from-tag to-tag)
-        (exit))))
+    (define (input-value-with-dialog from-tag)
+      (let ((to-tag (app-input-box from-tag)))
+        (if to-tag
+          (cons from-tag to-tag)
+          (exit))))
 
-  (define (substitute-tag template tag-ls)
-    (let loop ((tags (map input-value-with-dialog tag-ls))
-               (substituted-template template))
-      (if (null? tags)
-        substituted-template
-        (loop (cdr tags)
-              (regexp-replace-all
-                (string->regexp (string-append
-                                  "\\{\\{_var_:"
-                                  (caar tags)
-                                  "\\}\\}"))
-                substituted-template
-                (cdar tags))))))
-  (substitute-tag
-    (delete-_input_ template)
-    (collect-input-tag template)))
+    (define (substitute-tag template tag-ls)
+      (let loop ((tags (map input-value-with-dialog tag-ls))
+                 (substituted-template template))
+        (if (null? tags)
+          substituted-template
+          (loop (cdr tags)
+                (regexp-replace-all
+                  (string->regexp (string-append
+                                    "\\{\\{_var_:"
+                                    (caar tags)
+                                    "\\}\\}"))
+                  substituted-template
+                  (cdar tags))))))
 
-(define (expand-name template)
-  (regexp-replace-all
-    #/\{\{_name_\}\}/
-    template
+    (substitute-tag
+      (delete-_input_ template)
+      (collect-input-tag template)))
+
+  (define (expand-name template)
     (regexp-replace-all
-      #/.*\\(.*)\..*/
-      (editor-get-filename)
-      "$1")))
-
-(define (expand-expr template)
-  (regexp-replace-all
-    #/\{\{_expr_:(.+)\}\}/
-    template
-    (lambda (m)
-      (eval (read (open-input-string (rxmatch-substring m 1)))))))
-
-(define (append-indent template)
-  (define (get-current-indent)
-    (let ((indent (rxmatch-substring
-                    (rxmatch
-                      #/^\t+/
-                      (editor-get-row-string (editor-get-cur-row))))))
-      (if indent
-        (string-length indent)
-        #f)))
-  (let ((indent (get-current-indent)))
-    (if indent
+      #/\{\{_name_\}\}/
+      template
       (regexp-replace-all
-        #/\A\t+/
+        #/.*\\(.*)\..*/
+        (editor-get-filename)
+        "$1")))
+  
+  (define (expand-expr template)
+    (regexp-replace-all
+      #/\{\{_expr_:(.+)\}\}/
+      template
+      (lambda (m)
+        (eval (read (open-input-string (rxmatch-substring m 1)))))))
+  
+  (define (append-indent template)
+    (define (get-current-indent)
+      (let ((indent (rxmatch-substring
+                      (rxmatch
+                        #/^\t+/
+                        (editor-get-row-string (editor-get-cur-row))))))
+        (if indent
+          (string-length indent)
+          #f)))
+    (let ((indent (get-current-indent)))
+      (if indent
         (regexp-replace-all
-          #/^/
-          template
-          (make-string indent #\tab))
-        "")
-      template)))
-
-(define (goto-_cursor_)
-  (editor-search-string "\\{\\{_cursor_\\}\\}")
-  (editor-delete-selected-string))
-
-(let ((template (read-template)))
-  (if template
-    (begin
-      (editor-paste-string
-        (apply-chain
-          template
-          expand-var
-          expand-name
-          expand-expr
-          append-indent))
-      (goto-_cursor_)))))
+          #/\A\t+/
+          (regexp-replace-all
+            #/^/
+            template
+            (make-string indent #\tab))
+          "")
+        template)))
+  
+  (define (goto-_cursor_)
+    (editor-search-string "\\{\\{_cursor_\\}\\}")
+    (editor-delete-selected-string))
+  
+  (let ((template (read-template)))
+    (if template
+      (begin
+        (editor-paste-string
+          (apply-chain
+            template
+            expand-var
+            expand-name
+            expand-expr
+            append-indent))
+        (goto-_cursor_)))))
